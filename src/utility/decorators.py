ce89879e1f2d947dd785622a06afc1c923ef2328
Bastian Nispel <nispel@codingdesign.io>
2022-12-15 21:37:57 +0100

    added memory measurement decorator

--- src/utility/decorators.py
+++ src/utility/decorators.py
@@ -1,4 +1,5 @@
 from functools import wraps
+import tracemalloc
 import time
 
 def timeit(func):
@@ -11,3 +12,16 @@ def timeit(func):
         print(f'Function {func.__name__} Took {total_time:.4f} seconds')
         return result
     return timeit_wrapper
+
+def measure_memory(func):
+    @wraps(func)
+    def memory_wrapper(*args, **kwargs):
+        tracemalloc.start()
+        result = func(*args, **kwargs)
+        current, peak = tracemalloc.get_traced_memory()
+        print(f"\n\033[37mFunction Name       :\033[35;1m {func.__name__}\033[0m")
+        print(f"\033[37mCurrent memory usage:\033[36m {current / 10**6}MB\033[0m")
+        print(f"\033[37mPeak                :\033[36m {peak / 10**6}MB\033[0m")
+        tracemalloc.stop()
+        return result
+    return memory_wrapper
